d40c7ce0e9f23be8d95c813173c636194a217bce
morteza namvar <mnamvarcfd@gmail.com>
2023-08-08 19:33:48 -0400

    dynamo db

---
+++ Dynamodb/infra/lib/infra-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'cdk-dynamodb-Table',
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+  }
+}
