23b68dd4138cdc24cf440c39a40801c6d7dc6791
morteza namvar <mnamvarcfd@gmail.com>
2023-08-09 16:03:56 -0400

    dynamodb, s3, lambda, apigateway

---
+++ S3-API-Lambda-Dynamodb/lib/s3-api-lambda-dynamodb-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class S3ApiLambdaDynamodbStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+
+
+
+
+  }
+}
---
+++ S3-API-Lambda-Dynamodb/test/s3-api-lambda-dynamodb.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3ApiLambdaDynamodb from '../lib/s3-api-lambda-dynamodb-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3-api-lambda-dynamodb-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3ApiLambdaDynamodb.S3ApiLambdaDynamodbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
